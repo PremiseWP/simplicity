e1d12bcdb0cfb9fb8e6d432c2a28a78d793044de
Mario Vallejo <mario@vallgroup.com>
2016-08-11 10:17:59 -0500

    call loop on front page

--- front-page.php
+++ front-page.php
@@ -11,28 +11,7 @@ get_header();
 
 <section id="pwps-front-page">
 
-	<div class="pwps-the-loop">
-
-		<?php
-		/**
-		 * The loop. Check if we have posts and display them.
-		 */
-		if ( have_posts() ) :
-
-			while ( have_posts() ) : the_post();
-
-				get_template_part( 'content' );
-
-			endwhile;
-
-		else :
-
-			get_template_part( 'content', 'none' );
-
-		endif;
-		?>
-
-	</div>
+		<?php get_template_part( 'loop' ); ?>
 
 </section>
 
