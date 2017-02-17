715443a57b9e1f03d0abdb48d6e6c5034f40cf19
Mario Vallejo <mario@vallgroup.com>
2017-02-17 10:07:46 -0600

    added missing sidebar

--- front-page.php
+++ front-page.php
@@ -11,21 +11,25 @@ get_header();
 
 <section id="pwps-front-page"<?php pwps_the_section_class(); ?>>
 
-	<?php
-	if ( have_posts() ) :
+	<div class="pwps-loop <?php echo pwps_uses_sidebar() ? 'span8' : 'span12'; ?>">
+		<?php
+		if ( have_posts() ) :
 
-		while ( have_posts() ) : the_post();
+			while ( have_posts() ) : the_post();
 
-				get_template_part( 'content', pwps_get_post_format() );
+					get_template_part( 'content', pwps_get_post_format() );
 
-		endwhile;
+			endwhile;
 
-	else :
+		else :
 
-		get_template_part( 'content', 'none' );
+			get_template_part( 'content', 'none' );
 
-	endif;
-	?>
+		endif;
+		?>
+	</div>
+
+	<?php pwps_the_sidebar(); ?>
 
 </section>
 
