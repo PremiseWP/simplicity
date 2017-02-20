c0188413392d9622d263db3a16776bdc351f8642
Mario Vallejo <mario@vallgroup.com>
2017-02-20 14:36:10 -0600

    fixed footer id (prepended paps). added footer widgets.

--- footer.php
+++ footer.php
@@ -9,11 +9,35 @@
 
 	</div> <!-- Close the .sangreea-content tag -->
 
-	<footer id="footer">
+	<footer id="pwps-footer">
 		<div class="pwps-container">
+
+			<div class="premise-row">
+				<?php
+				$count = 0;
+				for ( $i=1; $i <= 4; $i++ ) {
+					if ( pwps_uses_sidebar( 'pwps-footer-'.$i.'-sidebar', false ) ) {
+						$count++;
+					}
+				}
+
+				$cols = ( 2 <= $count ) ? 'col'.$count : 'span12';
+
+				for ( $i=1; $i <= $count; $i++ ) {
+
+					?><div class="pwps-footer-sidebar <?php echo $cols; ?>">
+						<ul class="pwps-sidebar">
+							<?php dynamic_sidebar( 'pwps-footer-'.$i.'-sidebar' ); ?>
+						</ul>
+					</div><?
+				}
+				?>
+			</div>
+
 			<div class="premise-align-center copyright">
 				<p><?php echo pwps_get_copyright_text(); ?></p>
 			</div>
+
 		</div>
 	</footer>
 
