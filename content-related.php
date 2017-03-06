3df8f7e75c9e8fbeb5a0dea16869847a00ada029
Mario Vallejo <mario@vallgroup.com>
2017-03-06 09:51:27 -0600

    adjustments to content related

--- content-related.php
+++ content-related.php
@@ -16,16 +16,16 @@
 	</div>
 
 	<?php if ( has_post_thumbnail() ) : ?>
-		<div class="pwps-post-thumbnail">
+		<!-- <div class="pwps-post-thumbnail">
 			<a href="<?php the_permalink(); ?>" class="premise-block">
 				<?php the_post_thumbnail( 'pwps-thumbnail', array( 'class' => 'premise-responsive' ) ); ?>
 			</a>
-		</div>
+		</div> -->
 	<?php endif; ?>
 
-	<div class="pwps-post-excerpt">
+	<!-- <div class="pwps-post-excerpt">
 		<?php the_excerpt(); ?>
-	</div>
+	</div> -->
 
 	<?php pwps_the_post_meta(); ?>
 
