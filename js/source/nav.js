ab04aeeacdd5a3007ffeb7e65aaf4a97b89bbeec
Mario Vallejo <mario@vallgroup.com>
2017-03-06 09:51:05 -0600

    updated nav JS and CSS

--- js/source/nav.js
+++ js/source/nav.js
@@ -1,69 +1,32 @@
 (function($){
 
-	$( document ).ready( function() {
+	$(document).ready(pwpsNavigation);
 
+	function pwpsNavigation() {
 		var body = $( 'body' ),
 		header       = $( '#pwps-header' ),
 		navToggle    = $( '#pwps-nav-toggle-a' ),
-		navSearch    = $( '#pwps-nav-search-input' ),
 		navOverlay   = $( '.pwps-nav-wrapper' ),
-		pwpsContent   = $( '#pwps-content' ),
-		loopContainer = $( '.pwps-the-loop' ),
-		sidebarToggle = $( '.pwps-toggle-mobile-sidebar' );
+		sidebarToggle = $( '.pwps-toggle-mobile-sidebar' )
+		parents = $( '.pwps-nav-menu .menu-item-has-children' );
 
-		// Causes issues with some JS events. Need to rethink this
-		// if ( ! loopContainer.length ) {
-		// 	_conent = pwpsContent.html();
-		// 	pwpsContent.html( '<div class="pwps-the-loop">'+_conent+'</div>' );
-		// 	loopContainer = $( '.pwps-the-loop' );
-		// }
+		// bind our nav toggle button
+		if ( navToggle.length ) {
+			navToggle.click(function(){
+				console.log('hello');
+				( body.is( '.pwps-nav-active' ) ) ? closeNav() : openNav();
+			});
+		}
 
-		// activate the nav search
-		navToggle.click( pwpsInitNav );
+		if ( parents.length ) {
+			bindSubNav();
+		}
 
-		// bind the subnav navigation
-		pwpsSubnavigation();
-
-
-		var initialPage = ( loopContainer.length ) ? loopContainer[0].innerHTML : '';
-		// initiate nav search UI
-		function pwpsInitNav() {
-			// continue if reset was successful
-			if ( ! pwpsResetNav() ) return false;
-
-			header.addClass( 'pwps-nav-active' );
+		// open the nav
+		function openNav() {
 			body.addClass( 'pwps-nav-active' );
-			navSearch.focus();
 
-			// bind the search field
-			navSearch.keyup( function( e ) {
-
-				// if enter key is pressed
-				if ( e.keyCode == 13 ) {
-					header.removeClass( 'pwps-nav-active' );
-					body.removeClass( 'pwps-nav-active' );
-					navSearch.blur();
-					return false;
-				}
-
-				// reference our variables
-				var $this = $( this ),
-				s = $this.val();
-
-				setTimeout( function() {
-					// if string is at least 1 character long
-					if ( 1 <= s.length ) {
-							pwpsDoSearch( s );
-					}
-					else { // clean up loop and place inital content back
-						loopContainer.removeClass( 'pwps-nav-results' ).attr( 'data-pwps-nav-search', '' ).html( initialPage );
-						navOverlay.removeClass( 'loading' );
-					}
-					clearTimeout();
-				}, 300);
-
-			} );
-
+			// bind mobile sidebar toggle
 			sidebarToggle.click( function() {
 				var mobileSidebar = $( '.pwps-mobile-sidebar' );
 
@@ -83,60 +46,18 @@
 			navOverlay.off().click( function(e) {
 				e.stopPropagation();
 				if ( $( e.target ).is( '.pwps-nav-wrapper' ) ) {
-					console.log( e.target );
-					header.removeClass( 'pwps-nav-active' );
-					body.removeClass( 'pwps-nav-active' );
+					closeNav();
 				}
 			} );
-		}
+		};
 
-		// reset the nav. does not do nothing yet
-		function pwpsResetNav() {
-			if ( header.is( '.pwps-nav-active' ) ) {
-				header.removeClass( 'pwps-nav-active' );
-				return false;
-			}
-			else {
-				return true;
-			}
-		}
+		// close the nav
+		function closeNav() {
+			body.removeClass( 'pwps-nav-active' );
+		};
 
-		// preform the search and return results
-		function pwpsDoSearch( s ) {
-			s = s || '';
-
-			// check for s
-			if ( '' == s ) return false;
-
-			navOverlay.addClass( 'loading' );
-
-			// construct data object
-			var data = {
-				action: 'pwps_nav_search', 	// the ajax hook name
-				search: s, 					// what the user searched for
-			}
-
-			// call the ajax hook and pass data
-			$.post( '/wp-admin/admin-ajax.php', data, function( resp ) {
-				loopContainer.addClass( 'pwps-nav-results' ).attr( 'data-pwps-nav-search', s ).html( resp );
-				navOverlay.removeClass( 'loading' );
-				// pwpsLoadMorePostsAjax();
-			} );
-
-			return false;
-		}
-
-
-		/**
-		 * initaite the slide nav for our nav system
-		 *
-		 * @return {void} binds the slide menu animation to our mein nav
-		 */
-		function pwpsSubnavigation() {
-			var parents = $( '.pwps-nav-menu-container li.menu-item-has-children' );
-
-			if ( parents.length ) {
-				// fix propagation when clicking on the anchor of a parent li element
+		function bindSubNav() {
+			// fix propagation when clicking on the anchor of a parent li element
 				parents.find( 'a' ).click( function(e){ e.stopPropagation(); } );
 
 				var container = parents.parents( '.pwps-nav-menu-container' ),
@@ -180,10 +101,7 @@
 						} );
 					}
 				} );
-			}
-
 		}
-
-	} );
+	}
 
 }(jQuery));
