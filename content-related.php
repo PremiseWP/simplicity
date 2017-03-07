0b4517af3200b2a268c131fdd96edf25957c14b1
Mario Vallejo <mario@vallgroup.com>
2017-03-06 19:20:01 -0600

    removed commented code

--- content-related.php
+++ content-related.php
@@ -15,18 +15,6 @@
 		</a>
 	</div>
 
-	<?php if ( has_post_thumbnail() ) : ?>
-		<!-- <div class="pwps-post-thumbnail">
-			<a href="<?php the_permalink(); ?>" class="premise-block">
-				<?php the_post_thumbnail( 'pwps-thumbnail', array( 'class' => 'premise-responsive' ) ); ?>
-			</a>
-		</div> -->
-	<?php endif; ?>
-
-	<!-- <div class="pwps-post-excerpt">
-		<?php the_excerpt(); ?>
-	</div> -->
-
 	<?php pwps_the_post_meta(); ?>
 
 </article>
